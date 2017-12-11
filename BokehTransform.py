8f6d2babba9d1695bc81c1b0a1118550265a41c5
B. Clint Hall <hallbc@appstate.edu>
2017-12-10 21:37:11 -0500

    2d hists for transform plot

--- BokehTransform.py
+++ BokehTransform.py
@@ -1,11 +1,16 @@
 import numpy as np
 import pandas as pd
 from skimage import transform as tf
-from bokeh.io import curdoc, save
+from bokeh.io import curdoc
 from bokeh.layouts import row, widgetbox, column
-from bokeh.models.widgets import Slider, Button, TextInput
-from bokeh.plotting import figure, show
+from bokeh.models.widgets import  TextInput
+from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource
+from bokeh.palettes import brewer
+
+
+palette = brewer['Spectral'][11]
+palette[0] = '#FFFFFF'
 
 x_range = [-10, 10]
 y_range = [-10, 10]
@@ -15,43 +20,30 @@ y_range = [-10, 10]
 def k_hist():
     global k_hist_source
     p = figure(
-        plot_height=300,
-        plot_width=300,
-        tools='pan,box_zoom,reset')
-    p.quad(source=k_hist_source, left='left', right='right', top='top', fill_color="#036564", line_color="#033649", bottom=0)
+        plot_height=400,
+        plot_width=400,
+        tools='pan,box_zoom,reset, wheel_zoom')
+    p.quad(source=k_hist_source, left='left', right='right', top='top', fill_color="#036564", line_color="#033649", bottom='bottom')
     return p
 
 def v_hist():
     global v_hist_source
     p = figure(
-        plot_height=300,
-        plot_width=300,
-        tools='pan,box_zoom,reset')
-    p.quad(source=v_hist_source, left='left', right='right', top='top', fill_color="#036564", line_color="#033649", bottom=0)
+        plot_height=400,
+        plot_width=400,
+        tools='pan,box_zoom,reset, wheel_zoom')
+    p.quad(source=v_hist_source, left='left', right='right', top='top', fill_color="#036564", line_color="#033649", bottom='bottom')
     return p
 
 
-
-def orig_figure():
-    global orig_source
+def hist2d(src):
     p = figure(
-        plot_height=500,
-        plot_width=500,
-        x_axis_label=x_label,
-        y_axis_label=y_label,
-        tools='pan,box_zoom,reset')
-    p.circle(x=x_label, y=y_label, source=orig_source)
-    return p
-
-
-
-def create_figure():
-    global source
-    p = figure(
-        plot_height=500,
-        plot_width=500,
-        tools='pan,box_zoom,reset')
-    p.circle(x=x_label, y=y_label, source=source)
+        plot_height=400,
+        plot_width=400,
+        x_range=(src.data['x'][0], src.data['x'][0]+src.data['dw'][0]),
+        y_range=(src.data['y'][0], src.data['y'][0]+src.data['dh'][0]),
+        tools='pan,box_zoom,reset, wheel_zoom')
+    p.image(image='image', x='x', y='y', dw='dw', dh='dh', source=src, palette=palette)
     return p
 
 
@@ -60,29 +52,18 @@ def draw(attr, old, new):
     mat = at.params
     warped = mat.dot(sample)
     source.data = {x_label:warped[0], y_label:warped[1]}
-    orig_source.data = {x_label: sample[0], y_label: sample[1]}
+    #orig_source.data = {x_label: sample[0], y_label: sample[1]}
+    orig_hist2d.data=get_2dhist_coldatsrc_dict(sample, orig_fig)
+    transformed_hist2d.data=get_2dhist_coldatsrc_dict(warped, transformed_fig)
     hist, edges = np.histogram(source.data[x_label], bins=200)
-    k_hist_source.data=dict(top=hist, left=edges[:-1], right=edges[1:])
+    k_hist_source.data=dict(top=hist, left=edges[:-1], right=edges[1:], bottom=np.zeros(len(hist)))
     hist, edges = np.histogram(source.data[y_label], bins=200)
-    v_hist_source.data=dict(top=hist, left=edges[:-1], right=edges[1:])
+    v_hist_source.data=dict(top=edges[:-1], left=np.zeros(len(hist)), right=hist, bottom=edges[1:])
 
 
 
-def resample():
-    sample = data.sample(frac=float(fraction_ti.value)).values
-    sample = sample.transpose()
-    ones = np.ones((1,sample.shape[1]))
-    sample = np.vstack((sample, ones))
-    return sample
-
-def fraction_cb(attr, old, new):
-    global sample
-    sample = resample()
-    draw(attr, old, new)
 
 # Widgets
-fraction_ti = TextInput(title="sample fraction", value='0.01') #0.01
-fraction_ti.on_change('value', fraction_cb)
 
 rotation_ti = TextInput(title="rotation", value='-0.014')
 rotation_ti.on_change('value', draw)
@@ -100,22 +81,51 @@ y_label = data.columns[1]
 
 
 # Initial transform
-sample = resample()
+sample = data.sample(frac=0.1)
+sample = sample.values
+sample = sample.transpose()
+ones = np.ones((1,sample.shape[1]))
+sample = np.vstack((sample, ones))
+
 at = tf.AffineTransform(rotation=float(rotation_ti.value), shear=float(shear_ti.value))
 mat = at.params
 warped = mat.dot(sample)
 source = ColumnDataSource(data={x_label: warped[0], y_label: warped[1]})
-orig_source = ColumnDataSource(data={x_label: sample[0], y_label: sample[1]})
+#orig_source = ColumnDataSource(data={x_label: sample[0], y_label: sample[1]})
+
+def get_2dhist_coldatsrc_dict(sample, fig=None):
+    bins = [np.linspace(sample[1].min(), sample[1].max(), 200), np.linspace(sample[0].min(), sample[0].max(), 200)]
+    H, xedges, yedges = np.histogram2d(sample[1], sample[0], bins=bins)
+    H = np.log10(H + 1)
+
+    if fig is not None:
+        fig.x_range.start = xedges[0]
+        fig.x_range.end = xedges[-1]
+        fig.y_range.start = yedges[0]
+        fig.y_range.end = yedges[-1]
+    return dict(image=[H], x=[xedges[0]], y=[yedges[0]], dw=[xedges[-1]-xedges[0]], dh=[yedges[-1]-yedges[0]])
+
+
+orig_hist2d = ColumnDataSource(data=get_2dhist_coldatsrc_dict(sample))
+orig_fig = hist2d(orig_hist2d)
+
+transformed_hist2d = ColumnDataSource(data=get_2dhist_coldatsrc_dict(warped))
+transformed_fig = hist2d(transformed_hist2d)
+
 
 hist, edges = np.histogram(source.data[x_label], bins=200)
-k_hist_source = ColumnDataSource(data=dict(top=hist, left=edges[:-1], right=edges[1:]))
+k_hist_source = ColumnDataSource(data=dict(top=hist, left=edges[:-1], right=edges[1:], bottom=np.zeros(len(hist))))
 hist, edges = np.histogram(source.data[y_label], bins=200)
-v_hist_source = ColumnDataSource(data=dict(top=hist, left=edges[:-1], right=edges[1:]))
+v_hist_source = ColumnDataSource(data=dict(top=edges[:-1], left=np.zeros(len(hist)), right=hist, bottom=edges[1:]))
+
+
 
 
 # final ui setup
-controls = widgetbox([fraction_ti, rotation_ti, shear_ti], width=200)
-left_col = column(controls, k_hist(), v_hist())
-layout = row(orig_figure(), create_figure(), left_col,)
+controls = widgetbox([rotation_ti, shear_ti], width=200)
+
+mid_col = column(transformed_fig, k_hist())
+right_col = column(v_hist(), controls)
+layout = row(orig_fig, mid_col, right_col)
 curdoc().add_root(layout)
 
--- Hist2d.py
+++ Hist2d.py
@@ -1,7 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from bokeh.palettes import brewer
+from bokeh.models import ColumnDataSource
+
 from matplotlib.colors import LogNorm
+
+from bokeh.plotting import figure, show
 
 plt.close()
 x_range = [-3.226753235247827, 12.408753633883688]
@@ -13,9 +18,27 @@ data = pd.read_csv("all_tychoii_data.csv")
 data = data.dropna()
 data = data.set_index('name')
 
-range = [np.arange(x_range[0], x_range[1], 0.05), np.arange(y_range[0], y_range[1],0.05)]
+bins = [np.arange(x_range[0], x_range[1], 0.05), np.arange(y_range[0], y_range[1],0.05)]
 
-plt.hist2d(data.K, data.V, bins=range, range=[x_range, y_range], norm=LogNorm())
+"""
+plt.hist2d(data.K, data.V, bins=bins, range=[x_range, y_range], norm=LogNorm())
 plt.colorbar()
 plt.show()
+"""
+
+H, xedges, yedges = np.histogram2d(data.V, data.K, bins=bins)
+p = figure(x_range=x_range, y_range=y_range)
+
+H = np.log10(H + 1)
+
+# must give a vector of image data for image parameter
+palette = brewer['Spectral'][11]
+palette[0] = '#FFFFFF'
+
+hist2dsource = ColumnDataSource(data=dict(image=[H], x=[xedges[0]], y=[yedges[0]], dw=[xedges[-1]-xedges[0]], dh=[yedges[-1]-yedges[0]]))
+
+p.image(image='image', x='x', y='y', dw='dw', dh='dw', source=hist2dsource, palette=palette)
+
+show(p)
+
 
